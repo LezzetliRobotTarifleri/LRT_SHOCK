2acfd0448934a7bf6191e16d5341e582844686ba
Ömer Faruk Uçar <o.faruk.ucar@gmail.com>
2020-12-31 12:15:58 +0300

    update main.go location

--- config/port.go
+++ config/port.go
@@ -5,7 +5,7 @@ import "github.com/tarm/serial"
 // PortSetup function is used to define port settings
 func PortSetup() *serial.Config {
 	config := &serial.Config{
-		Name: "COM6",
+		Name: "COM10",
 		Baud: 9600,
 	}
 
